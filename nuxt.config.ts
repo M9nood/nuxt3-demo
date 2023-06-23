9ecbc2353929d80e098097446c8ffc45c0962468
toeiminal <toei@finnomena.com>
2023-06-23 14:42:41 +0700

    finish

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+// https://nuxt.com/docs/api/configuration/nuxt-config
+const CONTENT_URL = 'https://scontent-qa.finnomena.com'
+const LEAD_FORM_VERSION = '1.2.0-staging'
+export default defineNuxtConfig({
+    app: {
+        head: {
+            script: [
+                {
+                    hid: 'custom-form-editor-script',
+                    src: `${CONTENT_URL}/web-component/lead/form/${LEAD_FORM_VERSION}/finno-lead-form.min.js`
+                }
+            ],
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: `${CONTENT_URL}/web-component/lead/form/${LEAD_FORM_VERSION}/finno-lead-components-frontend.css`,
+                    type: 'text/css'
+                }
+            ]
+        }
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,35 @@
         <LabelTimer :timer="timer.time"></LabelTimer>
         <a @click="reload" class="btn">Refresh Data</a>
         <pre>{{ resData }}</pre>
+        <div>
+            <div style="width: 100%; display: flex">
+			<textarea v-model="formConfigString" cols="30" rows="5" style="width: 50%"></textarea>
+			<finno-form-editor style="width: 50%" :form-config-string="formConfigString"></finno-form-editor>
+            <!-- <finno-lead-form form-code="form1"></finno-lead-form> -->
+		</div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 const timer = useTimer()
+const formConfigString = `{
+    "inputs": [
+      {
+          "id": 1,
+          "name": "firstname",
+          "label": "ชื่อ",
+          "type": "text",
+          "required": true,
+          "placeholder": "กรุณาระบุชื่อ",
+          "warning_text": "ข้อมูลชื่อไม่ถูกต้อง",
+          "styles": {
+              "size": "small",
+              "font_weight": "bold"
+          }
+      }
+  ]
+  }`
 
 const { data: resData, refresh: refreshMe} = await useFetch('/api/me')
 
